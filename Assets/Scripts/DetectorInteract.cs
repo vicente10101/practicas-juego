be09dcd1ba8956f3d1d25dfa8d42c2faec8cb3ef
vicente10101 <vicenteferrari101@gmail.com>
2026-05-12 22:40:01 -0300

    funciono!

--- Assets/Scripts/DetectorInteract.cs
+++ Assets/Scripts/DetectorInteract.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DetectorInteract : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private interacuar enrango = null;
+    
+
+    public void AlInteractuar(InputAction.CallbackContext context)
     {
-        
+        if (context.performed)
+        {
+            enrango?.interact();
+        }
+    }
+//
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter2D(Collider2D collison)
+    {
+        if(collison.TryGetComponent(out interacuar interactable) && interactable.puedeinteract())
+        {
+            enrango=interactable;
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collison)
     {
-        
+        if(collison.TryGetComponent(out interacuar interactable) && interactable == enrango)
+        {
+            enrango=null;
+            
+        }
     }
 }
